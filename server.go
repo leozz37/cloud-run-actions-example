ae3fac279e283f80c0bf6d60e2792d4c95519ef6
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-10-03 00:39:16 -0300

    💚  Triggering CI

--- server.go
+++ server.go
@@ -15,7 +15,7 @@ type Phrase struct {
 
 func HelloWorld() Phrase {
 	return Phrase{
-		Text: "Hello World!",
+		Text: "Hello World",
 	}
 }
 
--- server_test.go
+++ server_test.go
@@ -7,7 +7,7 @@ import (
 func TestHelloWorld(t *testing.T) {
 	var expected Phrase
 
-	expected.Text = "Hello World!"
+	expected.Text = "Hello World"
 	result := HelloWorld()
 
 	if expected.Text != result.Text {
